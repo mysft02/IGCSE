823d807b3060c744faf82e33ec3beb442eca74a3
0767534208 <quanghuy010503@gmail.com>
2025-09-29 21:33:01 +0700

    fix

---
+++ Common/Constants/ErrorCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Constants
+{
+    public class ErrorCode
+    {
+        public const string BadRequest = "Bad Request";
+        public const string Unauthorized = "Unauthorized";
+        public const string Forbidden = "Forbidden";
+        public const string NotFound = "Not Found";
+        public const string ServerError = "Internal Server Error";
+    }
+}
