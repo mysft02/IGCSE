23f97c91f799d103097534ea55b19fe31b2ce2b1
@NET_10_NguyenTanDung <dungntse171487@fpt.edu.vn>
2025-10-13 13:50:35 +0700

    fix: remove unused import

--- Service/OpenAI/OpenAIEmbeddingsApiService.cs
+++ Service/OpenAI/OpenAIEmbeddingsApiService.cs
@@ -2,12 +2,6 @@
 using BusinessObject.Payload.Request.OpenAI;
 using BusinessObject.Payload.Response.OpenAI;
 using Common.Utils;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace Service.OpenAI
 {
