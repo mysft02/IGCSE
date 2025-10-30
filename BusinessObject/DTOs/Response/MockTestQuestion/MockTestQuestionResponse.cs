6225793438792df73ae288b593a73777725e818a
@NET_10_NguyenTanDung <dungntse171487@fpt.edu.vn>
2025-10-30 21:13:00 +0700

    fix: add mocktest question response

---
+++ BusinessObject/DTOs/Response/MockTestQuestion/MockTestQuestionResponse.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BusinessObject.DTOs.Response.MockTestQuestion
+{
+    public class MockTestQuestionResponse
+    {
+        public int MockTestQuestionId { get; set; }
+
+        public string QuestionContent { get; set; } = null!;
+
+        public string CorrectAnswer { get; set; } = null!;
+
+        public string? Image { get; set; }
+
+        [JsonIgnore] 
+        public string? ImageUrl { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
