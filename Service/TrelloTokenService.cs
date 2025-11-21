d7ee7f518d4510330d8f20ddc6e65c2fb7c9ca2b
0767534208 <quanghuy010503@gmail.com>
2025-11-21 20:52:30 +0700

    fix slot

--- Service/TrelloTokenService.cs
+++ Service/TrelloTokenService.cs
@@ -123,7 +123,7 @@ public class TrelloTokenService
             throw new Exception("Bạn chưa mua gói để tạo mới khoá học. Vui lòng mua gói trước.");
         }
 
-        if(createSlot.Slot <= createSlot.AvailableSlot)
+        if(createSlot.AvailableSlot <= 0)
         {
             throw new Exception("Bạn không còn suất tạo mới khoá học. Vui lòng mua thêm gói.");
         }
@@ -177,6 +177,11 @@ public class TrelloTokenService
 
                 if (ExtractTypeTrelloListContent(list.Name) == "Course")
                 {
+                    if (createSlot.AvailableSlot <= 0)
+                    {
+                        throw new Exception("Bạn không còn suất tạo mới khoá học. Vui lòng mua thêm gói.");
+                    }
+
                     //create course
                     course = await _courseService.CreateCourseForTrelloAsync(list.Name, trelloCardResponses, trelloToken.UserId, trelloToken);
                     
